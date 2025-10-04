4bbbe99f8853b880b82492bcc79ad0aef0ba3c4c
Dennis Reetz <dennis.reetz@gmail.com>
2025-10-04 12:55:21 +0200

    fix: session pool queue

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Backend for storing collected data from coffee counter.
 
 ## Changelog
 
+### 2025-10-04
+
+- Fix session pool queue
+
 ### 2025-09-27
 
 - Switch from sqlite to postgres
--- app/database.py
+++ app/database.py
@@ -2,15 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from app.config import settings
 
-from sqlalchemy.pool import NullPool
-
 url = f"postgresql://{settings.DB_USER}:{settings.DB_KEY}@{settings.DB_URL}/{settings.DB_SCHEMA}"
-con = create_engine(url, client_encoding="utf8", poolclass=NullPool)
-
 connect_args = {}
 engine = create_engine(url, connect_args=connect_args)
 
 
+# TODO Implement async queue
 def get_session():
     with Session(engine) as session:
-        yield session
+        try:
+            yield session
+        except Exception as err:
+            session.rollback()
+            print(err)
+        finally:
+            session.close()
